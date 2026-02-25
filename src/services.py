2065078aec877166e95a5b3891d75e63d9078603
yara@email <yara@email.com>
2026-02-25 15:21:06 +0300

    Add services layer to handle book operations

---
+++ src/services.py
@@ -0,0 +1,18 @@
+from database import add_book, get_books
+
+def add_new_book(title, author):
+    add_book(title, author)
+
+def list_books():
+    books = get_books()
+    return books
+
+def search_book_by_title(search_title):
+    books = get_books()
+    result = []
+
+    for book in books:
+        if search_title.lower() in book[1].lower():
+            result.append(book)
+
+    return result
